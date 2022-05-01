81d9273db7f66449debced1a7f5f2bfa38078060
ltactor <ltactor@hotmail.com>
2022-05-02 07:53:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/\/opt/\/mnt\/sda3\/docker/g' feeds/luci/applications/luci-app-docker/root/etc/docker/daemon.json
 rm -rf feeds/packages/utils/coremark
-#sed -i 's/(PKG_HASH|PKG_MD5SUM|PKG_MIRROR_HASH).*/\1:=skip/' feeds/packages/utils/containerd/Makefile
 sed -i 's/PKG_HASH.*/PKG_HASH:=skip/' feeds/packages/utils/containerd/Makefile
